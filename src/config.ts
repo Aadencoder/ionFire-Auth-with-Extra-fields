1d23558da04599e021aebc6edd82c628a4faf267
Aaden Sah <deltatechaadensah@gmail.com>
2018-04-29 13:04:16 +0545

    conf

--- src/config.ts
+++ src/config.ts
@@ -7,11 +7,11 @@ export class Config {
 
 export const firebaseConfig = {
 	fire: {
-		apiKey: "AIzaSyBhpCJTa7Liy_AU9Z9EmAY4PL2r2y8zlmA",
-		authDomain: "authuserextra.firebaseapp.com",
-		databaseURL: "https://authuserextra.firebaseio.com",
-		projectId: "authuserextra",
-		storageBucket: "authuserextra.appspot.com",
-		messagingSenderId: "713191646978"
+		apiKey: "",
+		authDomain: "",
+		databaseURL: "",
+		projectId: "",
+		storageBucket: "",
+		messagingSenderId: ""
 	}
 };
